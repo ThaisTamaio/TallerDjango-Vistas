ee4c49b1f6673a18e18af61dc1bcf3383ae8665f
Thais Tamaio <77994638+ThaisTamaio@users.noreply.github.com>
2022-08-27 12:58:55 -0500

    Taller 3 - Vistas
    Creo que ya está yuhu

--- measurements/models.py
+++ measurements/models.py
@@ -7,7 +7,6 @@ class Measurement(models.Model):
     value = models.FloatField(null=True, blank=True, default=None)
     unit = models.CharField(max_length=50)
     place = models.CharField(max_length=50)
-    dateTime = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return '%s %s' % (self.value, self.unit)
